d75f2bc557e34f60c8e9896f09ef19c788cf34ef
GFelixy <jogfy@163.com>
2019-11-27 23:42:07 +0800

    update vec3.h and vec3.cpp Signed-off-by: GFelixy <jogfy@163.com>

--- game/vec3.cpp
+++ game/vec3.cpp
@@ -7,7 +7,7 @@ vec3_t vec3_t::left_mul(mat33_t &matin) {
     memset(vecout.vector, 0, sizeof(double) * 3);
     for(int i = 0; i < 3; i++) {
         for(int j = 0; j < 3; j++) {
-            vecout.vector[i] += vector[i] * matin.matrix[i][j];
+            vecout.vector[i] += vector[j] * matin.matrix[i][j];
         }
     }
     return vecout;
@@ -17,7 +17,7 @@ mat33_t mat33_t::left_mul(mat33_t &matin) {
     memset(matout.matrix, 0, sizeof(double) * 9);
     for(int i = 0; i < 3; i++) {
         for(int j = 0; j < 3; j++) {
-            for(int k = 0; k < 3; k++){
+            for(int k = 0; k < 3; k++) {
                 matout.matrix[i][j] += matin.matrix[i][k] * matrix[k][j];
             }
         }
