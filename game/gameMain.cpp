ef7a57b2302794527955d7fa5fb8eee6ffd7e9b1
北海若 <shiruoxi2010@163.com>
2019-11-28 00:51:24 +0800

    As a first rendering

--- game/gameMain.cpp
+++ game/gameMain.cpp
@@ -11,43 +11,13 @@
 #include <wx/msgdlg.h>
 
 //(*InternalHeaders(gameDialog)
-#include <wx/settings.h>
-#include <wx/font.h>
-#include <wx/intl.h>
+#include <wx/wx.h>
 #include <wx/string.h>
+#include <wx/mstream.h>
 //*)
 
-//helper functions
-enum wxbuildinfoformat {
-    short_f, long_f };
-
-wxString wxbuildinfo(wxbuildinfoformat format)
-{
-    wxString wxbuild(wxVERSION_STRING);
-
-    if (format == long_f )
-    {
-#if defined(__WXMSW__)
-        wxbuild << _T("-Windows");
-#elif defined(__UNIX__)
-        wxbuild << _T("-Linux");
-#endif
-
-#if wxUSE_UNICODE
-        wxbuild << _T("-Unicode build");
-#else
-        wxbuild << _T("-ANSI build");
-#endif // wxUSE_UNICODE
-    }
-
-    return wxbuild;
-}
-
 //(*IdInit(gameDialog)
-const long gameDialog::ID_STATICTEXT1 = wxNewId();
-const long gameDialog::ID_BUTTON1 = wxNewId();
-const long gameDialog::ID_STATICLINE1 = wxNewId();
-const long gameDialog::ID_BUTTON2 = wxNewId();
+const long gameDialog::ID_STATICBITMAPMAIN = wxNewId();
 //*)
 
 BEGIN_EVENT_TABLE(gameDialog,wxDialog)
@@ -55,47 +25,21 @@ BEGIN_EVENT_TABLE(gameDialog,wxDialog)
     //*)
 END_EVENT_TABLE()
 
-gameDialog::gameDialog(wxWindow* parent,wxWindowID id)
-{
+gameDialog::gameDialog(wxWindow* parent, wxWindowID id) {
     //(*Initialize(gameDialog)
-    Create(parent, id, _("wxWidgets app"), wxDefaultPosition, wxDefaultSize, wxDEFAULT_DIALOG_STYLE, _T("id"));
-    BoxSizer1 = new wxBoxSizer(wxHORIZONTAL);
-    StaticText1 = new wxStaticText(this, ID_STATICTEXT1, _("Welcome to\nwxWidgets"), wxDefaultPosition, wxDefaultSize, 0, _T("ID_STATICTEXT1"));
-    wxFont StaticText1Font = wxSystemSettings::GetFont(wxSYS_DEFAULT_GUI_FONT);
-    if ( !StaticText1Font.Ok() ) StaticText1Font = wxSystemSettings::GetFont(wxSYS_DEFAULT_GUI_FONT);
-    StaticText1Font.SetPointSize(20);
-    StaticText1->SetFont(StaticText1Font);
-    BoxSizer1->Add(StaticText1, 1, wxALL|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 10);
-    BoxSizer2 = new wxBoxSizer(wxVERTICAL);
-    Button1 = new wxButton(this, ID_BUTTON1, _("About"), wxDefaultPosition, wxDefaultSize, 0, wxDefaultValidator, _T("ID_BUTTON1"));
-    BoxSizer2->Add(Button1, 1, wxALL|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 4);
-    StaticLine1 = new wxStaticLine(this, ID_STATICLINE1, wxDefaultPosition, wxSize(10,-1), wxLI_HORIZONTAL, _T("ID_STATICLINE1"));
-    BoxSizer2->Add(StaticLine1, 0, wxALL|wxEXPAND|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 4);
-    Button2 = new wxButton(this, ID_BUTTON2, _("Quit"), wxDefaultPosition, wxDefaultSize, 0, wxDefaultValidator, _T("ID_BUTTON2"));
-    BoxSizer2->Add(Button2, 1, wxALL|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 4);
-    BoxSizer1->Add(BoxSizer2, 0, wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 4);
-    SetSizer(BoxSizer1);
-    BoxSizer1->Fit(this);
-    BoxSizer1->SetSizeHints(this);
-    
-    Connect(ID_BUTTON1,wxEVT_COMMAND_BUTTON_CLICKED,(wxObjectEventFunction)&gameDialog::OnAbout);
-    Connect(ID_BUTTON2,wxEVT_COMMAND_BUTTON_CLICKED,(wxObjectEventFunction)&gameDialog::OnQuit);
+    Create(parent, wxID_ANY, _("WX Tower Project"), wxPoint(150, 120), wxDefaultSize, wxCAPTION | wxSYSTEM_MENU | wxCLOSE_BOX | wxBORDER, _T("wxID_ANY"));
+    SetClientSize(wxSize(800, 600));
+    BoxSizer = new wxBoxSizer(wxHORIZONTAL);
+    wxImage* image = new wxImage();
+    image->LoadFile(wxString::FromUTF8("res/9.png"), wxBITMAP_TYPE_PNG, -1);
+    StaticBitmapMain = new wxStaticBitmap(this, ID_STATICBITMAPMAIN, wxBitmap(*image), wxDefaultPosition, wxSize(800, 600), 0, _T("ID_STATICBITMAPMAIN"));
+    BoxSizer->Add(StaticBitmapMain, 1, wxALL | wxALIGN_CENTER_HORIZONTAL | wxALIGN_CENTER_VERTICAL, 0);
+    SetSizer(BoxSizer);
+    BoxSizer->SetSizeHints(this);
     //*)
 }
 
-gameDialog::~gameDialog()
-{
+gameDialog::~gameDialog() {
     //(*Destroy(gameDialog)
     //*)
-}
-
-void gameDialog::OnQuit(wxCommandEvent& event)
-{
-    Close();
-}
-
-void gameDialog::OnAbout(wxCommandEvent& event)
-{
-    wxString msg = wxbuildinfo(long_f);
-    wxMessageBox(msg, _("Welcome to..."));
 }
--- game/gameMain.h
+++ game/gameMain.h
@@ -12,40 +12,28 @@
 
 //(*Headers(gameDialog)
 #include <wx/sizer.h>
-#include <wx/stattext.h>
-#include <wx/statline.h>
-#include <wx/button.h>
+#include <wx/statbmp.h>
 #include <wx/dialog.h>
 //*)
 
-class gameDialog: public wxDialog
-{
+class gameDialog: public wxDialog {
     public:
 
-        gameDialog(wxWindow* parent,wxWindowID id = -1);
+        gameDialog(wxWindow* parent, wxWindowID id = -1);
         virtual ~gameDialog();
 
     private:
 
         //(*Handlers(gameDialog)
-        void OnQuit(wxCommandEvent& event);
-        void OnAbout(wxCommandEvent& event);
         //*)
 
         //(*Identifiers(gameDialog)
-        static const long ID_STATICTEXT1;
-        static const long ID_BUTTON1;
-        static const long ID_STATICLINE1;
-        static const long ID_BUTTON2;
+        static const long ID_STATICBITMAPMAIN;
         //*)
 
         //(*Declarations(gameDialog)
-        wxButton* Button1;
-        wxStaticText* StaticText1;
-        wxBoxSizer* BoxSizer2;
-        wxButton* Button2;
-        wxStaticLine* StaticLine1;
-        wxBoxSizer* BoxSizer1;
+        wxStaticBitmap* StaticBitmapMain;
+        wxBoxSizer* BoxSizer;
         //*)
 
         DECLARE_EVENT_TABLE()
