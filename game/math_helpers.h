ddb030b719928d9e37d49b37aeecc6d588ac5714
北海若 <shiruoxi2010@163.com>
2019-11-28 16:15:43 +0800

    Math Helpers

---
+++ game/math_helpers.h
@@ -0,0 +1,10 @@
+#include <cmath>
+#include <algorithm>
+using namespace std;
+inline double mapd(double v, double a, double b, double na, double nb) {
+    return (v - a) / (b - a) * (nb - na) + na;
+}
+
+inline double clampd(double v, double a, double b) {
+    return min(b, max(v, a));
+}
