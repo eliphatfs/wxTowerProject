2c8f6329115a73173905587a680e49abf611a341
北海若 <shiruoxi2010@163.com>
2019-11-29 19:12:12 +0800

    Game IO

---
+++ game/game_io.cpp
@@ -0,0 +1,32 @@
+#include "world.h"
+#include "player.h"
+#include <fstream>
+#include <iostream>
+using namespace std;
+
+void dump_state(const char * path) {
+    ofstream file(path, ios::binary);
+    player_t plr = get_player();
+    file.write((char *)&plr, sizeof(plr));
+    for (int i = 0; i < 1000; i++) {
+        for (int j = 0; j < 1000; j++) {
+            world_block_t block = get_world_block(i, j);
+            file.write((char *)&block, sizeof(world_block_t));
+        }
+    }
+}
+
+void load_state(const char * path) {
+    ifstream file(path, ios::binary);
+    if (file.fail())
+        return;
+    player_t& plr = get_player();
+    file.read((char *)&plr, sizeof(plr));
+    for (int i = 0; i < 1000; i++) {
+        for (int j = 0; j < 1000; j++) {
+            world_block_t block;
+            file.read((char *)&block, sizeof(world_block_t));
+            set_world_block(i, j, block);
+        }
+    }
+}
---
+++ game/game_io.h
@@ -0,0 +1,7 @@
+#ifndef GAME_IO_H
+#define GAME_IO_H
+
+void dump_state(const char * path);
+void load_state(const char * path);
+
+#endif
