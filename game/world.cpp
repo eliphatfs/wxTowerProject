932e543f0ac75d770a4f11c6691575859b855289
北海若 <shiruoxi2010@163.com>
2019-12-01 14:05:56 +0800

    Fix world.cpp SEGFAULT

--- game/world.cpp
+++ game/world.cpp
@@ -4,10 +4,10 @@ using namespace std;
 
 static world_block_t world_map[1000][1000];
 world_block_t get_world_block(int x, int y) {
-    if (x < 0 || x >= 1000 || y < 0 || y >= 1000) cout << "out of space in get_world_block" << endl;
+    if (x < 0 || x >= 1000 || y < 0 || y >= 1000) { cout << "out of space in get_world_block" << endl; return space; }
     return world_map[x][y];
 }
 void set_world_block(int x, int y, world_block_t block) {
-    if (x < 0 || x >= 1000 || y < 0 || y >= 1000) cout << "out of space in set_world_block" << endl;
+    if (x < 0 || x >= 1000 || y < 0 || y >= 1000) { cout << "out of space in set_world_block" << endl; return;}
     world_map[x][y] = block;
 }
